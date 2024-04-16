7b27727299eda5bd156e4ae4d691b55a4a1384c3
AlexandreRouma <alexandre.rouma@gmail.com>
2024-04-16 23:58:16 +0200

    more work

---
+++ airfoil.py
@@ -0,0 +1,12 @@
+class Airfoil:
+    def __init__(self, polars: dict):
+        pass
+
+    def cl(self, Re: float, alpha: float):
+        pass
+
+    def cd(self, Re: float, alpha: float):
+        pass
+
+    def cm(self, Re: float, alpha: float):
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from airfoil import *
 
 # Configuration
 OUTPUT_DIR  = 'output'
@@ -6,3 +7,15 @@ OUTPUT_DIR  = 'output'
 # Create output directory if it does not exist
 if not os.path.exists(OUTPUT_DIR):
     os.mkdir(OUTPUT_DIR)
+
+# Load airfoil polar
+POLARS = {
+    0.1e6: 'airfoil_polars/Polar_0.1e6_clark_y.txt',
+    0.2e6: 'airfoil_polars/Polar_0.2e6_clark_y.txt',
+    0.5e6: 'airfoil_polars/Polar_0.5e6_clark_y.txt',
+    1e6: 'airfoil_polars/Polar_1e6_clark_y.txt',
+    2e6: 'airfoil_polars/Polar_2e6_clark_y.txt',
+    5e6: 'airfoil_polars/Polar_5e6_clark_y.txt',
+    10e6: 'airfoil_polars/Polar_10e6_clark_y.txt',
+}
+af = Airfoil(POLARS)
